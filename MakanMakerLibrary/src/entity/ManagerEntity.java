8ce29eedaf11f4ac78378b5833fc15e861990bd0
Ismahfaris <ismahfaris@gmail.com>
2018-03-07 15:33:41 +0800

    Recommit on edits to Enitity as Requested
    Please see changes to Entities & let me know if there are any issues. Thank you.
    Best Regards
    Faris

--- MakanMakerLibrary/src/entity/ManagerEntity.java
+++ MakanMakerLibrary/src/entity/ManagerEntity.java
@@ -21,7 +21,7 @@ public class ManagerEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long managerId;
     
     @Column(unique = true, nullable = false)
